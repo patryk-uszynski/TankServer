0ce9bcba1456f7ac00594fcc8f94540fabcd42ef
Patryk Uszyński <uszynski.patryk@gmail.com>
2017-03-14 02:39:00 +0100

    quickfix

--- run.php
+++ run.php
@@ -12,7 +12,7 @@ $http = new HttpServer(new WsServer($testServer));
 
 $server = IoServer::factory($http, 1337);
 
-$ticksPerSecond = !empty($argv[1]) ? : 15;
+$ticksPerSecond = !empty($argv[1]) ? $argv[1] : 15;
 
 $server->loop->addPeriodicTimer(1 / $ticksPerSecond, function () use ($testServer) {
 	$testServer->tick();
